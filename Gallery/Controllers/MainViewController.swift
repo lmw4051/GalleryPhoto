d1969a59def4106fea9dec104b02711b3d108862
David <dvdlee41@gmail.com>
2021-01-20 02:02:10 +0800

    Adding PhotoCell and UIView+Layout files and setup collectionView to show image

--- Gallery/Controllers/MainViewController.swift
+++ Gallery/Controllers/MainViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class MainViewController: UIViewController {
+  // MARK: - Properties
+  var galleryItems = [GalleryItem]()
+  
+  private lazy var collectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    collectionView.dataSource = self
+    collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.reuseIdentifier)    
+    return collectionView
+  }()
+  
+  // MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .red
     
+    getItems()
+    setupCollectionView()
+  }
+  
+  fileprivate func getItems() {
     Service.shared.loadPhotos(perPage: 10, pageNumber: 15) { (items, error) in
       if let error = error {
         print("Failed to fetch:", error)
         return
       }
+            
+      guard let items = items else { return }
       
-      print(items)
+      for item in items {
+        self.galleryItems.append(item)
+      }
+      
+      print(self.galleryItems)
+      
+      DispatchQueue.main.async {
+        self.collectionView.reloadData()
+      }
     }
+  }
+  
+  fileprivate func setupCollectionView() {
+    view.addSubview(collectionView)
+    collectionView.fillSuperview()
+  }
+}
+
+extension MainViewController: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {    
+    return galleryItems.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCell.reuseIdentifier, for: indexPath) as! PhotoCell
+    cell.gallery = galleryItems[indexPath.item]
+    return cell
   }
 }
---
+++ Gallery/Views/PhotoCell.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoCell.swift
+//  Gallery
+//
+//  Created by David on 2021/1/20.
+//  Copyright © 2021 David. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class PhotoCell: UICollectionViewCell {
+  // MARK: - Properties
+  var gallery: GalleryItem! {
+    didSet {
+      print(gallery.urls)
+      imageView.sd_setImage(with: URL(string: gallery.urls.regular))
+    }
+  }
+  
+  static let reuseIdentifier = "PhotoCell"
+  
+  let imageView = UIImageView(cornerRadius: 0)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .blue
+    imageView.fillSuperview()
+    addSubview(imageView)
+    imageView.fillSuperview()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
