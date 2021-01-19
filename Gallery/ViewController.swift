d1d78daee6c0626868d9564fb8cc92e4a7084de0
David <dvdlee41@gmail.com>
2021-01-19 19:42:17 +0800

    Initial Commit

---
+++ Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery
+//
+//  Created by David on 2021/1/19.
+//  Copyright © 2021 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
